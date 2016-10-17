3421c3afec71365fafdc98fd245d767fe0e3ad46
Martijn Maas <martijn.maas@huygens.knaw.nl>
2016-10-17 11:27:21 +0200

    Use a database on the host.

--- neo4j-cluster-test/run_as_docker.sh
+++ neo4j-cluster-test/run_as_docker.sh
@@ -2,5 +2,5 @@
 
 docker build -t clustertest .
 if [ $? -eq 0 ]; then
-	docker run --detach --publish=9080:8080 --publish=9081:8081 clustertest
+	docker run --detach --publish=9080:8080 --publish=9081:8081 --volume=$PWD/testdatabase:/testdatabase clustertest
 fi
